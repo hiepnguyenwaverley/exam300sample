e8f6cd8ae24fe11408bad8af838c6684f7f0d294
HiepNguyen <hiep.nguyen@waverleysoftware.com>
2023-08-28 11:12:27 +0700

    sample 3 line of triangle

---
+++ PractiseProject/Exam1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static bool IsTriangle(int a, int b, int c)
+{
+    return a + b > c && a + c > b && b + c > a;
+}
+
+
+Console.WriteLine("Nhập ba giá trị a, b, c:");
+
+Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("c = ");
+int c = int.Parse(Console.ReadLine());
+
+if (IsTriangle(a, b, c))
+{
+    Console.WriteLine("Ba giá trị này có thể là ba cạnh của một tam giác.");
+}
+else
+{
+    Console.WriteLine("Ba giá trị này không thể là ba cạnh của một tam giác.");
+}
+
