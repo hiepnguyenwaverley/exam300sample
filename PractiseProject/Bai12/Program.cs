2255b72d86c58f13a47481582180411b7842a332
khang.to <khang.to@wsvietnam.com>
2023-09-11 11:28:34 +0700

    update bai12_2

--- PractiseProject/Bai12/Program.cs
+++ PractiseProject/Bai12/Program.cs
@@ -5,5 +5,6 @@ if (n /10 >= 10 && n % 100 >= 10)
 { 
     Console.WriteLine("\n=>" + n / 10 + " " + n % 100); 
 }
-Console.WriteLine("\n"+n/10+" "+"0"+n%100);// chia cho /10 lay 2 chu so dau tien , chia cho %100 lay 2 so du
+Console.WriteLine("\n"+n/10+" "+"0"+n%100);// chia cho /10 lay 2 chu so dau tien , chia cho %100 lay 2 so du ,
+                                           // neu 2 chu so co 0 phia truoc thi van in 
 Console.ReadKey();
