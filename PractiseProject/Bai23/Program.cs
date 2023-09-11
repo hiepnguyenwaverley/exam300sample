6c41477f9b7c7e565b98585ddc5bd1205cfb314e
khang.to <khang.to@wsvietnam.com>
2023-09-11 16:47:54 +0700

    bai 23

---
+++ PractiseProject/Bai23/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("nhap so nguyen duong n :");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i < n; i++) // vong lap for , chay tu 1 den so n 
+{
+    if(i%3==0)
+    {
+        Console.WriteLine("Cac so chia het cho 3 :"+i);
+    }
+}
+Console.WriteLine("-");//khong co thi in dau - 
+Console.ReadKey();
