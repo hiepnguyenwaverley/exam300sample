ae099d94019e1d55fa8affa14b45de2139d71032
khang.to <khang.to@wsvietnam.com>
2023-09-20 15:52:59 +0700

    update bai21

--- PractiseProject/Bai21/Program.cs
+++ PractiseProject/Bai21/Program.cs
@@ -1,16 +1,17 @@
-﻿Console.Write("Nhap so nguyen km da di: ");
+﻿int money = 0;
+Console.Write("Nhap so nguyen km da di: ");
 int km=Convert.ToInt32(Console.ReadLine());
 if (km <= 1) 
 {
-    km = km * 12000;
+    money = km * 12000;
 }
 else if (km <= 30)
 {
-    km = 1 * 12000 +(km - 1) * 10000; 
+    money = 1 * 12000 +(km - 1) * 10000; 
 }
 else
 {
-    km = 1 * 12000 + 30 * 10000 + (km -1- 30) * 9000;
+    money = 1 * 12000 + 30 * 10000 + (km -1- 30) * 9000;
 }
-Console.WriteLine("So tien can tra cho taxi la :" + km +" dong");
+Console.WriteLine("So tien can tra cho taxi la :" + money +" dong");
 Console.ReadLine();
