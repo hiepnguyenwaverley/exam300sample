6f89bf4feaa7f88a2d46993c65cc11b89f3bc9db
khang.to <khang.to@wsvietnam.com>
2023-09-20 16:13:32 +0700

    bai26

---
+++ PractiseProject/Bai26/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum = 0;
+Console.WriteLine("Nhap so nguyen duong n : ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.Write("Cac so chia het cho 3 va 5 :");
+for(int i=1;i<=n;i++)
+{
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write(" " + i);
+        sum = sum + i;
+    }
+}
+Console.WriteLine("\nTong cac so chia het cho ca 3 va 5 :" + sum);
+Console.ReadLine();
