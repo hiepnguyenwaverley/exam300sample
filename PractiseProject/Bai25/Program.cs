4244bd2278e260d33f901b84bd13e089ebbb893f
khang.to <khang.to@wsvietnam.com>
2023-09-20 15:32:03 +0700

    bai25

---
+++ PractiseProject/Bai25/Program.cs
@@ -0,0 +1,14 @@
+﻿//so chan la so chia het cho 2
+int sum = 0;
+Console.Write("Nhap so nguyen duong n: ");
+int n= Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<=n; i++)
+{
+    if(i%2==0)
+    {
+        Console.Write(" "+i);
+        sum=sum+i;  
+    }
+}
+Console.WriteLine("\nTong cac so chan tu 1 den n :" + sum);
+Console.ReadLine();
