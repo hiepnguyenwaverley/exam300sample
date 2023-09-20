27b2593de96c99a66982a9083195d94025bbee31
khang.to <khang.to@wsvietnam.com>
2023-09-20 16:03:26 +0700

    update bai25

--- PractiseProject/Bai25/Program.cs
+++ PractiseProject/Bai25/Program.cs
@@ -2,13 +2,27 @@
 int sum = 0;
 Console.Write("Nhap so nguyen duong n: ");
 int n= Convert.ToInt32(Console.ReadLine());
-for(int i=1; i<=n; i++)
+//for(int i=1; i<=n; i++)
+//{
+//    if(i%2==0)
+//    {
+//        Console.Write(" "+i);
+//        sum=sum+i;  
+//    }
+//}
+//CACH KHAc DUNG MANG
+List<int> list = new List<int>();
+for (int i = 1; i <= n; i++)
 {
-    if(i%2==0)
+    if( i % 2 == 0)
     {
-        Console.Write(" "+i);
-        sum=sum+i;  
+        list.Add(i);
     }
+}
+list.ForEach(a => Console.Write(a + " "));
+for(int i = 0; i < list.Count;i++)
+{
+    sum = sum + list[i];    
 }
 Console.WriteLine("\nTong cac so chan tu 1 den n :" + sum);
 Console.ReadLine();
