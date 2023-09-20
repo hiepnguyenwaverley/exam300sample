a55dcc435015403b4e9166f5d7fa1259b99578eb
khang.to <khang.to@wsvietnam.com>
2023-09-20 14:55:22 +0700

    bai22

---
+++ PractiseProject/Bai22/Program.cs
@@ -0,0 +1,21 @@
+﻿int money;
+Console.WriteLine("Nhap so Kw/h dien da su dung ");
+int kw = Convert.ToInt32(Console.ReadLine());
+if (kw <= 50)//50kw=600d
+{
+    money = kw * 600;
+}
+else if (kw <= 100)//them 50kw 
+{
+    money = 50 * 600 + (kw - 50) * 800;
+}
+else if (kw <= 200)
+{
+    money = 50 * 600 + 50 * 800 + (kw - 50 - 50) * 1100;
+}
+else
+{
+    money = 50 * 600 + 50 * 800 + 100 * 1100 + (kw - 50 - 50 - 100) * 1500;
+}
+Console.WriteLine("So tien dien can tra :" + money);
+Console.ReadKey();
