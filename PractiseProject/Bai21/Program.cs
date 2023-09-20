6c19557ad74ebede36d9bbe32867db5270f98645
khang.to <khang.to@wsvietnam.com>
2023-09-20 10:53:21 +0700

    bai21

--- PractiseProject/Bai21/Program.cs
+++ PractiseProject/Bai21/Program.cs
@@ -1,17 +1,19 @@
 ﻿int tien;
 Console.WriteLine("Nhap so km da di : ");
 int km = Convert.ToInt32(Console.ReadLine());   
-if (km <= 1)//1km = 12000d
+
+if (km <= 1)
 {
     tien = km * 12000;
 }
-else if( km > 2 && km <= 30 )// km > 2 && km <= 30km = 10000d
+else if(km > 1 && km <= 30 )
 {
     tien = 1*12000+(km-1)*10000;
     
-}else
+}
+else
 {
     tien = 1*12000 + 29 * 10000 + (km - 30) * 9000;
 }
-Console.WriteLine("So tien phai tra la :"+tien+"d");
+Console.WriteLine("So tien phai tra la : "+tien);
 Console.ReadLine();
