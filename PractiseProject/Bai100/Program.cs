ee791c327c61c90324fddbfce1d7a2ca7a49463d
khang.to <khang.to@wsvietnam.com>
2023-10-02 12:01:34 +0700

    bai100

--- PractiseProject/Bai100/Program.cs
+++ PractiseProject/Bai100/Program.cs
@@ -1,31 +1,35 @@
-﻿Console.Write("Nhap so nguyen duong n : ");
-int n=Convert.ToInt32(Console.ReadLine());
-var stringArray = Console.ReadLine().Split(" ");
-int[]a=new int[n];
-for(int i=0;i<n;i++)
+﻿using System;
+
+Console.WriteLine("Nhap so n : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[]a= new int[n];
+var stringArray=Console.ReadLine().Split(' ');
+for (int i = 0; i < n; i++)
 {
     a[i]= int.Parse(stringArray[i]);
-    
 }
 int vt1 = 0;
 int vt2 = 0;
-int max = a[0];
-int min = a[0];
-for(int i = 0;i<n;i++)
+int max = 0;
+int min = 0;
+for (int i = 0;i < n;i++)
 {
     if (a[i]>max)
     {
-        max=a[i];
-        vt1=i;
+        max = a[i];
+        vt1 = i;
     }
     if (a[i]<min)
     {
         min = a[i];
-        vt2= i;
-
-    }    
-} 
-Console.WriteLine("Max: " + max+" Vi tri " +vt1);
-Console.WriteLine("Min: " + min + " Vi tri " + vt2);
+        vt2 = i;
+    }
+}
+Console.WriteLine("Max: " + max + " Vi Tri: " + vt1);
+Console.WriteLine("Min: " + min + " Vi Tri: " + vt2);
 Console.ReadLine();
+
+
+
+
 
