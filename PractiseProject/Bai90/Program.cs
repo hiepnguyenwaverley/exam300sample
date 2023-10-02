4ee471de1d1e0e5692378f4c5cc268f6df23506e
khang.to <khang.to@wsvietnam.com>
2023-10-02 17:14:52 +0700

    update bai90

--- PractiseProject/Bai90/Program.cs
+++ PractiseProject/Bai90/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Nhap so x: ");
+double x=Convert.ToInt32(Console.ReadLine());
+Console.Write("Nhap so n : ");
+double n=Convert.ToInt32(Console.ReadLine());
+Console.Write("Nhap so k: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Nhap so a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+double F = 0;
+for(int i=0; i<k; i++)
+{
+   F=F+a*Math.Pow(x,n);
+}
+Console.WriteLine("ket qua:"+F);
+Console.ReadLine();
