c7a890b15cb9313ceef1cdd286a0c18b026e7c94
khang.to <khang.to@wsvietnam.com>
2023-10-04 15:07:31 +0700

    bai87

---
+++ PractiseProject/Bai87/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Nhap so nguyen n : ");
+int n=Convert.ToInt32(Console.ReadLine());
+if(n<=0)
+{
+    Console.WriteLine("Moi ban nhap so nguyen");
+}
+else if(n%5==0)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
+Console.ReadLine();
